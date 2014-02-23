4e47d688c5f492a3ad2c1c9b822341172fa70f5f
mahdi pedramrazi <mpedramrazi@ebay.com>
2014-02-23 02:38:54 -0800

    develoing a common module for loading commono modules. never use global variables.

---
+++ modules/common.js
@@ -0,0 +1,14 @@
+"use strict";
+
+var common = {
+	winston: require('winston')
+};
+
+common.logger = new (common.winston.Logger)({
+    transports: [
+      new (common.winston.transports.Console)(),
+      new (common.winston.transports.File)({ filename: 'server.log' })
+    ]
+  });
+
+module.exports = common;
